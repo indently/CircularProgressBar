1ab374186f1a702fa60353404ea697c335eaeb1c
federicocotogno <federicosou@fastmail.se>
2021-07-12 09:34:16 +0200

    Added the code

--- App.js
+++ App.js
@@ -1,11 +1,40 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
+import React, { useState } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import CircularProgress from 'react-native-circular-progress-indicator';
 
 export default function App() {
+  const [value, setValue] = useState(0);
+
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Remember to subscribe to Code Palace!</Text>
+
+      <CircularProgress
+        radius={90}
+        value={85}
+        textColor='#222'
+        fontSize={20}
+        valueSuffix={'%'}
+        inActiveStrokeColor={'#2ecc71'}
+        inActiveStrokeOpacity={0.2}
+        inActiveStrokeWidth={6}
+        duration={3000}
+        onAnimationComplete={() => setValue(50)}
+      />
+
+      <CircularProgress
+        radius={100}
+        value={value}
+        textColor='#222'
+        fontSize={20}
+        valueSuffix={'%'}
+        activeStrokeColor={'tomato'}
+        inActiveStrokeOpacity={0.2}
+        duration={4000}
+      />
+
       <StatusBar style="auto" />
     </View>
   );
